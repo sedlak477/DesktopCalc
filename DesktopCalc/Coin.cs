8c14eb0d25002d15cceefe48883a6c186d3326ec
sedlak477 <michi.sedlak@gmail.com>
2017-12-07 20:03:52 +0100

    Add crypto currency data to calculator

---
+++ DesktopCalc/Coin.cs
@@ -0,0 +1,10 @@
+﻿namespace DesktopCalc
+{
+    public class Coin
+    {
+        public string Name { get; set; }
+        public string Ticker { get; set; }
+        public double? FiatValue { get; set; }
+        public double? BtcValue { get; set; }
+    }
+}
--- DesktopCalc/MainWindow.xaml.cs
+++ DesktopCalc/MainWindow.xaml.cs
@@ -1,4 +1,9 @@
 ﻿using NCalc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.ObjectModel;
+using System.Net;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
@@ -7,11 +12,39 @@ namespace DesktopCalc
 {
     public partial class MainWindow : System.Windows.Window
     {
+        private ObservableCollection<Coin> _coins = new ObservableCollection<Coin>();
 
         public MainWindow()
         {
             InitializeComponent();
+            datagrid_cryptoData.ItemsSource = _coins;
         }
+
+        private void _loadCoins(string currency)
+        {
+            try
+            {
+                string url = "https://api.coinmarketcap.com/v1/ticker/";
+                if (!currency.Equals("usd", StringComparison.InvariantCultureIgnoreCase))
+                    url += "?convert=" + currency.ToUpper();
+                using (WebClient cl = new WebClient())
+                {
+                    JArray data = JArray.Parse(cl.DownloadString(url));
+                    _coins.Clear();
+                    foreach (JObject o in data)
+                        _coins.Add(new Coin()
+                        {
+                            Name = (string)o["name"],
+                            Ticker = (string)o["symbol"],
+                            BtcValue = (double?)o["price_btc"],
+                            FiatValue = (double?)o["price_" + currency.ToLower()]
+                        });
+                }
+            }
+            catch { }
+        }
+
+        // --- Event Handler ---
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
@@ -33,14 +66,29 @@ namespace DesktopCalc
             if (e.ChangedButton == MouseButton.Middle) Close();
         }
 
-        private void Window_Deactivated(object sender, System.EventArgs e)
+        private void Window_Deactivated(object sender, EventArgs e)
         {
             BorderBrush = new SolidColorBrush(Colors.Gray);
         }
 
-        private void Window_Activated(object sender, System.EventArgs e)
+        private void Window_Activated(object sender, EventArgs e)
         {
             BorderBrush = new SolidColorBrush(Colors.Black);
+        }
+
+        private void Reload_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            _loadCoins(((ComboBoxItem)combobox_currency.SelectedItem).Content as string);
+        }
+
+        private void Value_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            textbox_expression.Text += ((sender as DataGridCell).Content as TextBlock).Text;
+        }
+
+        private void Clear_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            textbox_expression.Text = "";
         }
     }
 }
